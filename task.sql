5319b23a350afc241a431740aff5c2ae58ba2bac
Orest Yakymiv <akimivorest@gmail.com>
2025-04-15 17:39:49 +0300

    change the name of parametr function

--- task.sql
+++ task.sql
@@ -2,11 +2,11 @@ USE ShopDB;
 
 -- Create your stored procedure here
 DELIMITER //
-CREATE PROCEDURE get_warehouse_product_inventory(IN CompositionID INT)
+CREATE PROCEDURE get_warehouse_product_inventory(IN WarehouseID INT)
 BEGIN
   SELECT Products.Name, ProductInventory.WarehouseAmount
   FROM Products
   JOIN ProductInventory ON Products.ID = ProductInventory.ProductID
-  WHERE ProductInventory.ID = CompositionID;
+  WHERE ProductInventory.WarehouseID = WarehouseID;
 END //
 DELIMITER ;
