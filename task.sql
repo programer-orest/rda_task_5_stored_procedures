4a0791d78ba78bb22457bfb17dd44234e9a9533d
Orest Yakymiv <akimivorest@gmail.com>
2025-04-15 17:30:58 +0300

    Create procedure for Composition

--- task.sql
+++ task.sql
@@ -1,3 +1,12 @@
 USE ShopDB; 
 
 -- Create your stored procedure here
+DELIMITER //
+CREATE PROCEDURE get_warehouse_product_inventory(IN CompositionID INT)
+BEGIN
+  SELECT Products.Name, ProductInventory.WarehouseAmount
+  FROM Products
+  JOIN ProductInventory ON Products.ID = ProductInventory.ProductID
+  WHERE ProductInventory.ID = CompositionID;
+END //
+DELIMITER ;
